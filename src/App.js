aeb267bb7b21d2e748dfdc5c6c6c76087b40661a
unknown <hadirahiminejad88@gmail.com>
2023-03-14 10:32:24 +0330

    creating translate app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import './App.css';
+import Field from './components/field.js';
+import Translate from './components/translate.js';
+import Languages from './components/languages.js';
+
+function App() {
+  const [language, setLanguage] = useState('es');
+  const [text, setText] = useState('');
+  return (
+    <div>
+      <Field  onChange={setText}/>
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
+    </div>
+  );
+}
+
+export default App;
